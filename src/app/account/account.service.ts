49c1df33b6bced940d0be82a927ab207e99d0312
Manish Kumar <yadavmanishk2511@gmail.com>
2024-08-31 17:12:47 +0530

    #apiUrl fixed

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -3,19 +3,20 @@ import { Login } from '../shared/models/login';
 import { Observable, map } from 'rxjs';
 import { AccountResponse } from '../shared/models/AccountResponse';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-
+  private apiurl = environment.apiUrl;
   constructor(
     private http: HttpClient
   ) { }
 
   login(model: Login): Observable<any> {
-   
-    return this.http.post<AccountResponse>(`/account/login`, model)
+    var data = this.http.post<AccountResponse>(`${this.apiurl}Account/Login`, model)
+    return data;
       //.pipe(
       //  map((user: AccountResponse) => {
       //    debugger
---
+++ src/environments/environment.production.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  appUrl: 'http://localhost:5159',
+  userKey: 'safeStashAppUser'
+};
